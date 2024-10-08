3fef1ac33868da0fc0f63d1ace0ae024b93d06a9
InNoxIn <InNoxIn@proton.me>
2024-10-08 10:56:39 +0100

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...otherArgs) {
+
+    let firstArr = arr;
+    otherArgs.forEach(arg => {
+        let index = firstArr.indexOf(arg);
+        while (index !== -1) {
+            firstArr.splice(index, 1);
+            index = firstArr.indexOf(arg)
+        }
+    });
+    return firstArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
