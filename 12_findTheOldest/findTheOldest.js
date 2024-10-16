d50a92b1b53572f6fc9b5b222f997ada4c8efc3e
InNoxIn <innoxin@proton.me>
2024-10-16 22:40:52 +0100

    solved with help

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        // Determine current year if the person is still alive
+        const currentYear = (new Date()).getFullYear();
+        
+        // Calculate the age of each person
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        
+        // Return the person with the greater age
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
